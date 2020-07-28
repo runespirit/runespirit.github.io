bd6d5cc7b73386c542b4d930196a681a051901ac
Pedro Oliveira <phyxsius21@hotmail.com>
2020-07-28 17:27:54 +0200

    Update Discord permissions num.

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 
     //Top bar "shortcuts".
     $(".topBar-GetRunespirit").on("click", function () {
-        window.open("https://discordapp.com/oauth2/authorize?client_id=594125647182299147&scope=bot&permissions=273472", "_blank");
+        window.open("https://discordapp.com/oauth2/authorize?client_id=594125647182299147&scope=bot&permissions=314368", "_blank");
     });
 
     $(".topBar-Commands").click(function () {
